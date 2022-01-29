b4dfabbd3c65440ac250101af57513731947c565
Jörgen Krieger <jorgen@rox.nl>
2022-01-29 23:30:19 +0100

    Added spaceship theme

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# Install spaceship prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${$ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+
 # Install ZSH syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
