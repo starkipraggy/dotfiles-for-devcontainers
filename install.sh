c94897482e9541a92bec00a455ca7517fe4a4403
Jörgen Krieger <jorgen@rox.nl>
2022-01-30 00:01:58 +0100

    Added bat

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ git clone https://github.com/spaceship-prompt/spaceship-prompt.git $HOME/.oh-my-
 # Install ZSH syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+# Update debian dependancies
+if command -v apt-get &> /dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y bat
+fi
+
 # Create symlinks
 ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -nfs $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
