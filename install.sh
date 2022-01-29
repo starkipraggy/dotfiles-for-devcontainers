4ae3e23c2b92345fe2fd6dcda88c2ff6658ffad8
Jörgen Krieger <jorgen@rox.nl>
2022-01-29 23:32:18 +0100

    Updated install syntax

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # Install spaceship prompt
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git ${$ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt --depth=1
-ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git $HOME/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
 
 # Install ZSH syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Create symlinks
-ln -s $HOME/dotfiles/.aliases $HOME/.aliases
+ln -nfs $HOME/dotfiles/.aliases $HOME/.aliases
 ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -nfs $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
